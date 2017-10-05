4477bfbca01cb6c4d34d6f6b9e24f04705529a57
klynch <klynch@newroads.org>
2017-10-05 00:19:47 -0400

    This still needs some work but I saw a coupple of things I am missing but have no idea how to continue

--- Person.java
+++ Person.java
@@ -10,17 +10,39 @@ private String occupation;
 public Person(){
 this.age = 1;
 }
+  
 
 public Person(String name, int age, String gender, String occupation){
 this.name = name;
-this.age = "age: " age;
-this.gender = "gender: "gender;
-this.occupation = "occupation: "occupation;
+this.age = age;
+this.gender = gender;
+this.occupation = occupation;
+  
 }
 
 public String getName(){
   return this.name;
 }
-publuic void setname(String name){
+publuic void setName(String name){
   this.name = name;
 }
+
+public int getAge(){
+  return this.age;
+}
+publuic void setAge(String age){
+  this.age = age;
+}
+public String getGender(){
+  return this.gender;
+}
+publuic void setGender(String gender){
+  this.gender = gender;
+}
+public String getOccupation(){
+  return this.Occupation;
+}
+publuic void setOccupation(String occupation){
+  this.occupation = occupation;
+}
+
--- Runner.java
+++ Runner.java
@@ -2,10 +2,10 @@ public class Runner{
 
   public static void main (String[] args){
     
-   // SplashScreen splash = new SplashScreen();
+    SplashScreen splash = new SplashScreen();
     
-    //splash.splashScreenStart();
-   // splash.splashScreenEnd();
+    splash.splashScreenStart();
+    splash.splashScreenEnd();
     
     Person Kayden = new Person();
     Kayden.name = "Kayden";
