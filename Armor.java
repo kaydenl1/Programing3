454766d195ba4ac9bd9f0e2821584eb24c8558da
klynch <klynch@newroads.org>
2017-10-11 20:21:54 -0400

    I got evrything to wrork except for the array which is really close I think

--- Armor.java
+++ Armor.java
@@ -2,13 +2,68 @@ public class Armor{
 
   private String name;
   private int armorClass;
-  public String size;
-  public String weight;
+  private int size;
+  private int weight;
+  private double wear;
+  private boolean isUsable;
 
-  public Armor{
-    
+  public Armor(){
+    this.wear = 0;
+    this.isUsable = true;
   }
   
+  public Armor(String name, int armorClass, int size, int weight, double wear, boolean isUsable){
+   this.name = name;
+   this.armorClass = armorClass;
+   this.size = size;
+   this.weight = weight;
+   this.wear = wear;
+   this.isUsable = isUsable;
+  }
   
-  
+public String getName(){
+  return this.name;
 }
+public void setName(String name){
+  this.name = name;
+}
+public int getArmorClass(){
+  return this.armorClass;
+}
+public void setArmorClass(int armorClass){
+  this.armorClass = armorClass;
+}
+public int GetSize(){
+  return this.size;
+}
+public void setSize(int size){
+  this.size = size;
+}
+public int getWeight(){
+  return this.weight;
+}
+public void setWeight(int weight){
+  this.weight = weight;
+}
+  public double getWear(){
+  return this.wear;
+}
+public void setWear(double wear){
+  this.wear = wear;
+}
+  public boolean getIsUsable(){
+    return this.isUsable;
+  }
+public void setIsUsable(boolean isUseable){
+  this.isUsable = isUsable;
+}
+
+  
+  public String toString(){
+ 
+   return "You found: " + this.name + "It is a level " + this.armorClass + "balistic protection and is: " + this.size + "and weighs: " + this.weight + "lb" + " its condition is: " + this.wear + this.isUsable;
+    
+  }
+    
+}
+  
---
+++ Armor1A.java
@@ -0,0 +1,15 @@
+
+public class Armor1A extends Armor{
+
+public PistolArmor(){
+  super.setName("Level 1A Armor");
+  super.setArmorClass(1);
+  super.setSize(1)
+  super.setWeight(2)
+}
+  
+  public String toString(){
+    return super.toString();
+  }
+  
+}
