c65508c754b4d8dc53223dbd837ad161cb521985
klynch <klynch@newroads.org>
2017-11-02 12:45:47 -0400

    Added the begenting of a Story class

---
+++ Story.java
@@ -0,0 +1,58 @@
+public class Story{
+
+  private String roomName;
+  private int roomDiscription;
+  private boolean isLocked;
+
+  public Armor(){
+  boolean isLocked = false;
+  }
+  
+  public Armor(String name, int armorClass, int size, int weight, double wear, boolean isUsable){
+   this.roomName = roomName;
+   this.roomDiscription = roomDiscription;
+   this.isLocked = isLocked;
+  }
+  
+  /*
+  
+public String getName(){
+  return this.name;
+}
+public void setName(String name){
+  this.name = name;
+}
+public int getArmorClass(){
+  return this.armorClass;
+}
+public void setArmorClass(int armorClass){
+  this.armorClass = armorClass;
+}
+public int GetSize(){
+  return this.size;
+}
+public void setSize(int size){
+  this.size = size;
+}
+public int getWeight(){
+  return this.weight;
+}
+public void setWeight(int weight){
+  this.weight = weight;
+}
+  public double getWear(){
+  return this.wear;
+}
+public void setWear(double wear){
+  this.wear = wear;
+}
+  public boolean getIsUsable(){
+    return this.isUsable;
+  }
+public void setIsUsable(boolean isUseable){
+  this.isUsable = isUsable;
+}
+
+*/
+
+}
